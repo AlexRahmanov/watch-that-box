bfef363d1b8925aed29438aa07df3c4f17a250cc
AlexRahmanov <86209350+AlexRahmanov@users.noreply.github.com>
2021-12-05 13:09:05 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+        document.getElementById("button1").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "250px";
+
+        });
+
+        document.getElementById("button2").addEventListener("click", function change(){
+
+            document.getElementById("box").style.background = "blue";
+ 
+        });
+
+        document.getElementById("button3").addEventListener("click", function(){
+
+            document.getElementById("box").style.className = "fade";
+
+        });
+
+        document.getElementById("button4").addEventListener("click", function(){
+
+        document.getElementById("box").style.height = "150px";
+
+        });
+        
